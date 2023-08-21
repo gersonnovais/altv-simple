256acff2b5d2463118337210c5de64c3851b41e3
Pablo Oliveira <55354322+xipanca@users.noreply.github.com>
2023-08-20 22:31:08 -0400

    fanancial refactor
    validade negative values

--- src/core/server/module/extends/player/financial.ts
+++ src/core/server/module/extends/player/financial.ts
@@ -9,37 +9,42 @@ declare module 'alt-server' {
         removeToWallet(value: number): boolean;
         addToBank(value: number): boolean;
         bankWithdraw(value: number): boolean;
+        depositToBank(value: number): boolean;
     }
 }
 
 alt.Player.prototype.addToWallet = function addToWallet(value: number) {
-    if (!this.wallet) this.wallet = value;
-    else this.wallet += value;
-
+    if (value < 0) return false;
+    if (!this.wallet) this.wallet = 0;
+    this.wallet += value;
     return true;
 };
 
 alt.Player.prototype.removeToWallet = function removeToWallet(value: number) {
-    if (this.wallet < 0) this.wallet = 0;
-    if (!this.wallet) this.wallet = value;
-    else this.wallet -= value;
-
+    if (value < 0 || !this.wallet || this.wallet - value < 0) return false;
+    this.wallet -= value;
     return true;
 };
 
 alt.Player.prototype.addToBank = function addToBank(value: number) {
-    if (!this.bank) this.bank = value;
+    if (value < 0 || !this.wallet || this.wallet - value < 0) return false;
+    if (!this.bank) this.bank = 0;
     this.bank += value;
     this.wallet -= value;
-
     return true;
 };
 
 alt.Player.prototype.bankWithdraw = function bankWithdraw(value: number) {
-    if (!this.bank) this.bank = value;
-    if (this.bank - value < 0) return false;
+    if (value < 0 || !this.bank || this.bank - value < 0) return false;
     this.bank -= value;
     this.wallet += value;
+    return true;
+};
 
+alt.Player.prototype.depositToBank = function depositToBank(value: number) {
+    if (value < 0 || !this.wallet || this.wallet < value) return false;
+    if (!this.bank) this.bank = 0;
+    this.wallet -= value;
+    this.bank += value;
     return true;
 };
